c297732802b709ad86f9141b93c51d758e23b1cd
Berkay Orhan <berkayorhan@hotmail.se>
2025-12-11 20:53:01 +0100

    chore: update Next.js and related dependencies to version 16.0.8 in bun.lock and clean up next.config.mjs formatting

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    cacheComponents: true,
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'cdn.myanimelist.net',
-        },
-      ],
-      qualities: [75, 80],
-    },
-  };
-  
-  export default nextConfig;
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.myanimelist.net',
+      },
+    ],
+    qualities: [75, 80],
+  },
+}
+
+export default nextConfig;
